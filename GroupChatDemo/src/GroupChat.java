c7079bc7c7ee45c14ac2451de9c0f4b7689e6977
JakeLandowski <jakelandowski@gmail.com>
2018-10-04 11:22:57 -0700

    Hardcoded the ip address and port

--- GroupChatDemo/src/GroupChat.java
+++ GroupChatDemo/src/GroupChat.java
@@ -7,61 +7,60 @@ public class GroupChat
     private static final String TERMINATE = "Exit";
     static String name;
     static volatile boolean finished = false;
+    public static final String MULTICAST = "224.0.0.0";
+    public static final int PORT = 1234;
+
     public static void main(String[] args)
     {
-        if (args.length != 2)
-            System.out.println("Two arguments required: <multicast-host> <port-number>");
-        else
+        try
         {
-            try
+            InetAddress group = InetAddress.getByName(MULTICAST);
+//            int port = Integer.parseInt(args[1]);
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            name = sc.nextLine();
+            MulticastSocket socket = new MulticastSocket(PORT);
+
+            // Since we are deploying
+            socket.setTimeToLive(0);
+
+            //this on localhost only (For a subnet set it as 1)
+            socket.joinGroup(group);
+            Thread t = new Thread(new ReadThread(socket,group,PORT));
+
+            // Spawn a thread for reading messages
+            t.start();
+
+            // sent to the current group
+            System.out.println("Start typing messages...\n");
+            while(true)
             {
-                InetAddress group = InetAddress.getByName(args[0]);
-                int port = Integer.parseInt(args[1]);
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Enter your name: ");
-                name = sc.nextLine();
-                MulticastSocket socket = new MulticastSocket(port);
-
-                // Since we are deploying
-                socket.setTimeToLive(0);
-                //this on localhost only (For a subnet set it as 1)
-                socket.joinGroup(group);
-                Thread t = new Thread(new
-                        ReadThread(socket,group,port));
-
-                // Spawn a thread for reading messages
-                t.start();
-
-                // sent to the current group
-                System.out.println("Start typing messages...\n");
-                while(true)
+                String message;
+                message = sc.nextLine();
+                if(message.equalsIgnoreCase(GroupChat.TERMINATE))
                 {
-                    String message;
-                    message = sc.nextLine();
-                    if(message.equalsIgnoreCase(GroupChat.TERMINATE))
-                    {
-                        finished = true;
-                        socket.leaveGroup(group);
-                        socket.close();
-                        break;
-                    }
-                    message = name + ": " + message;
-                    byte[] buffer = message.getBytes();
-                    DatagramPacket datagram = new
-                            DatagramPacket(buffer,buffer.length,group,port);
-                    socket.send(datagram);
+                    finished = true;
+                    socket.leaveGroup(group);
+                    socket.close();
+                    break;
                 }
+                message = name + ": " + message;
+                byte[] buffer = message.getBytes();
+                DatagramPacket datagram = new
+                        DatagramPacket(buffer,buffer.length,group,PORT);
+                socket.send(datagram);
             }
-            catch(SocketException se)
-            {
-                System.out.println("Error creating socket");
-                se.printStackTrace();
-            }
-            catch(IOException ie)
-            {
-                System.out.println("Error reading/writing from/to socket");
-                        ie.printStackTrace();
-            }
+        }
+        catch(SocketException se)
+        {
+            System.out.println("Error creating socket");
+            se.printStackTrace();
+        }
+        catch(IOException ie)
+        {
+            System.out.println("Error reading/writing from/to socket");
+                    ie.printStackTrace();
         }
     }
 }
